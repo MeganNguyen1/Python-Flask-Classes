48a1ebe597cb4dd801621f11c0af6a4c9a1dcb59
MeganNguyen1 <nguyenmegan973@gmail.com>
2023-08-22 19:33:21 -0500

    first commit

---
+++ basic.py
@@ -0,0 +1,17 @@
+# print("Hello, Megan")
+
+name = input("Enter your name:")
+
+print('megan')
+print('''megan''')
+print("""megan""")
+
+# we can use single quotes, double quotes, triple single quotes,
+# triple double quotes in the print statement in python
+
+# multiline comment 
+
+'''
+hi my name is megan
+i am studying in first grade 
+'''
