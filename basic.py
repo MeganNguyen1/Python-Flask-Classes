7454118c2fa267f882d05d39bb15c61e95f301d7
MeganNguyen1 <nguyenmegan973@gmail.com>
2023-08-24 19:24:47 -0500

    data types

--- basic.py
+++ basic.py
@@ -1,10 +1,10 @@
 # print("Hello, Megan")
 
-name = input("Enter your name:")
+# name = input("Enter your name:")
 
-print('megan')
-print('''megan''')
-print("""megan""")
+# print('megan')
+# print('''megan''')
+# print("""megan""")
 
 # we can use single quotes, double quotes, triple single quotes,
 # triple double quotes in the print statement in python
@@ -15,3 +15,39 @@ print("""megan""")
 hi my name is megan
 i am studying in first grade 
 '''
+# integer = all the numbers from negitive to positive side
+# float = all the numbers with decimal points
+# double = very big decimal point number
+# string = group of characters (alphabets, numbers, symbols, spaces)
+# bool = True/False
+# char= 'a' '0', '-', ':', '(' = single characters are store in char
+
+# variable = it is a container to store something in the memory
+
+num = 45
+print(num)
+
+num = 5.60
+print(num)
+print(type (num))
+
+num = 55890.734758475485734857485749857848573498574857768749090714589409490573
+print(num)
+
+mychar =  '6'
+print(mychar)
+
+mystr = "Megan and Urvashi"
+print(mystr)
+print(type (mystr))
+
+variable = True
+print(variable)
+print(type(variable))
+
+# variable naming conventions = how you can make a variable
+# 1. you csn write any alphabet lowercase and uppercase in the variable
+# example: num, number string, megan, urvashi, UVRASHI
+# 2. Numbers can be added in a variable but not in the first place
+# example: megan45, go457tu,
+# 45tyu --> this is wrong
