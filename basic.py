279c4c7ad97bf6809c49c7a99d3619712201388d
MeganNguyen1 <nguyenmegan973@gmail.com>
2023-08-26 10:42:50 -0500

    varaible naming convention

--- basic.py
+++ basic.py
@@ -24,26 +24,26 @@ i am studying in first grade
 
 # variable = it is a container to store something in the memory
 
-num = 45
-print(num)
+# num = 45
+# print(num)
 
-num = 5.60
-print(num)
-print(type (num))
+# num = 5.60
+# print(num)
+# print(type (num))
 
-num = 55890.734758475485734857485749857848573498574857768749090714589409490573
-print(num)
+# num = 55890.734758475485734857485749857848573498574857768749090714589409490573
+# print(num)
 
-mychar =  '6'
-print(mychar)
+# mychar =  '6'
+# print(mychar)
 
-mystr = "Megan and Urvashi"
-print(mystr)
-print(type (mystr))
+# mystr = "Megan and Urvashi"
+# print(mystr)
+# print(type (mystr))
 
-variable = True
-print(variable)
-print(type(variable))
+# variable = True
+# print(variable)
+# print(type(variable))
 
 # variable naming conventions = how you can make a variable
 # 1. you csn write any alphabet lowercase and uppercase in the variable
@@ -51,3 +51,50 @@ print(type(variable))
 # 2. Numbers can be added in a variable but not in the first place
 # example: megan45, go457tu,
 # 45tyu --> this is wrong
+# comments = that part of code which is not executed by python and python ignores it
+
+# m = "Megan"
+# print(m)
+
+# M = "Megan"
+# print(M)
+
+# number = 34
+# print(number)
+
+# m23 = "Megan"
+# print(m23)
+
+# m45M = "Megan"
+# print(m45M)
+
+# number% = 34
+# print(number%)
+
+# number^ = 56
+# print(number^)
+
+# num^ber = 4
+# print(num^ber)
+
+# number_ = 23
+# print(number_)
+
+# _number = 4
+# print(_number)
+
+# num_ber = 5
+# print(num_ber)
+
+# num ber = 45
+# print(num ber)
+
+# 1. you can write any alphabet including Lowercase and uppercase in the variable
+# example: num, number, string, megan, uvrashi, UVRASHI
+# 2. numbers can be added in a variable but not at the first place
+# example: megan45, go457tu
+# 3. symbols are not allowed in the variable except _(underscore)
+# example: num_, _num, n_um
+# 45tyu --> this is wrong
+# 4. spaces are not allowed in the variable
+# example: nu m --> this is wrong
