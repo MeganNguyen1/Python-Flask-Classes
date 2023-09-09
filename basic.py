f2498da53410fa59d3937cd144d42a145674aa08
MeganNguyen1 <nguyenmegan973@gmail.com>
2023-09-09 10:33:18 -0500

    If else is done

--- basic.py
+++ basic.py
@@ -315,9 +315,72 @@ mystr = "I am having a big task"
 # else:
 #     print("none")
 
-if 4 > 6 and not(8 > 5):
-    print("one")
-elif not(4 > 5) or not(5 == 5):
-    print("two")
+# if 4 > 6 and not(8 > 5):
+#     print("one")
+# elif not(4 > 5) or not(5 == 5):
+#     print("two")
+# else:
+#     print("none")
+
+# if 4 > 3:
+#     print("three")
+# if 4 > 2:
+#     print("four")
+# if 5 > 4:
+#     print("five")
+# elif 4 == 4:
+#     print("six")
+# elif 4 > 3:
+#     print("seven")
+# else:
+#     print("default")
+
+# if 4 < 3:
+#      print("three")
+# if 4 > 2:
+#      print("four")
+# if 5 < 4:
+#      print("five")
+# elif 4 != 4:
+#      print("six")
+# elif 4 > 3:
+#      print("seven")
+# else:
+#      print("default")
+
+# x = 3 
+# y = 12
+# if x <= y and x > 0:
+#     print("three")
+# if x > y or y < 11:
+#     print("four")
+# if not(x < 4) and y < 24:
+#     print("five")
+# elif y != 4:
+#     print("six")
+# elif y >= x:
+#     print("seven")
+# else:
+#     print("default")
+
+x = 12
+y = 3
+z = 11
+if (x == 2 and x < 3):
+     print(x)
+if x != 5:
+    print("whatever")
+if x != 5 and y >= 5 and z <= 13:
+     print("its gonna happen")
+if z != 0 or x == 2:
+     print("you say")
+if (not(y < 10)):
+    print("you say")
+elif (x < 10 or x < 5):
+     print("okkkkkkk")
+elif (y < 10 or y <= 0):
+    print("pikachu")
+elif(z == 0 or y ==5):
+    print(pikaboo)
 else:
-    print("none")
+    print("Default value")
