56bc732b81d28f6a461d7370647b118884665d0b
MeganNguyen1 <nguyenmegan973@gmail.com>
2023-09-23 10:38:38 -0500

    Math and Random Module

--- basic.py
+++ basic.py
@@ -564,10 +564,86 @@ mylist = [1, 90.5, "Megan", 'd', True]
 # 3. True 90.5
 # 4. 90.5 1
 
-print(mylist[-1:-6:-2])
+# print(mylist[-1:-6:-2])
 
-print(mylist[-1:-4:-1])
+# print(mylist[-1:-4:-1])
 
-print(mylist[-1:-5:-3])
+# print(mylist[-1:-5:-3])
 
-print(mylist[])
+# print(mylist[-4:-6:-1])
+
+# modules in python
+
+# 1. math module
+
+import math as m
+# maximum = max([10, 20, 11, 3, 9, 0, 12])
+
+# print(maximum)
+
+# minimum = min([10, 20, 11, 3, 9, 0, 12])
+
+# print(minimum)
+
+# absolute = abs(-7.6)
+
+# print(absolute)
+
+# absolute = abs(7.6)
+
+# print(absolute)
+
+# ceiling = m.ceil(-10.9)
+
+# print(ceiling)
+
+# floor_value = m.floor(-10.9)
+
+# print(floor_value)
+
+# random module 
+
+import random as r
+
+# num = r.randint(5.3, 16.4)
+
+# print(num)
+
+# start and end are both included
+
+# start and end point both should be integers
+
+# num = r.randrange(5, 19)
+
+# print(num)
+
+# start index is included but not end index
+
+# start and end point both should be integers
+
+# num = r.random()
+
+# print(num)
+
+# random function gives random numbers from 0 to 1
+
+# num = r.choice([9, 6, 2, 8, 10, 12])
+
+# print(num)
+
+# choice function picks a random choice from a list of elements.
+
+# element = r.choice("megan")
+
+# print(element)
+
+# choice function also works for strings
+
+element = r.uniform(5, 17)
+
+print(element)
+
+# 1. uniform function gives a random floating point number in a given range
+# 2. uniform function doesn't include end index
+
+print(dir(m))
