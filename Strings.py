de7b721ea8b46ee100012cea2c4eb097634d34d6
MeganNguyen1 <nguyenmegan973@gmail.com>
2023-10-01 10:49:22 -0500

    Relational, Logical Identical, Membership Operators

---
+++ Strings.py
@@ -0,0 +1,112 @@
+# strings
+
+# mystr = "Megan"
+# print(mystr[2])
+
+# print(mystr[1])
+
+# print(mystr[0])
+
+# print(print[3])
+
+# mystr = "I am having a big task"
+
+# print(mystr[5])
+# print(mystr[11])
+
+# we cannot change/update a string's value
+# mystr[5] = 'p'
+
+mystr = "I am having a big task"
+# #big
+# print(mystr[14:17])
+
+# havi
+# print(mystr[5:9])
+
+# g a big
+# print(mystr[10:17])
+
+# m hav
+# print(mystr[3:8])
+
+#avin
+# print(mystr[6:10])
+
+# print(mystr[2:5])
+
+# print(mystr[18:])
+
+# print(mystr[0:11])
+
+# if you are not putting any start index, it will be starting from 0 index
+# if you are not putting any end index, it will go up to the last element
+# print(mystr[:])
+
+# print(mystr[0:5:2])
+# print(mystr[2:9:2])
+# print(mystr[3:6:2])
+
+# print(mystr[2::2])
+# print(mystr[4::4])
+
+# print(mystr[::5])
+
+# print(mystr[-7])
+# print(mystr[5])
+# print(mystr[-17])
+
+# print(mystr[-5:-2])
+
+# print(mystr[-8:-1:2])
+# print(mystr[-5::2])
+
+# print(mystr[-2:-7:-2])
+
+# print(mystr[-8:-7:-2])
+
+# print(mystr[-6:-1:2])
+
+# print(mystr[-4:0:-4])
+
+# print(mystr[-9:1:-3])
+
+# print(mystr[-7:15:-1])
+
+# print(mystr[7:-14])
+
+# Strings Practice
+
+mystring = "Megan studies in first grade"
+# print(mystring[2:13])
+
+# print(mystring[-26:-15])
+
+# print(mystring[2:-15])
+
+# print(mystring[-26:13])
+
+# print(mystring[9:22:3])
+ 
+# print(mystring[-19:-6:3])
+
+# print(mystring[9:-6:3])
+
+# print(mystring[-19:22:3])
+
+# print(mystring[-5:-14:-2])
+
+mystr = "Football is a very famous game"
+# print(mystr[2:-12:2])
+
+# print(mystr[15:30:2])
+
+# print(mystr[-15::2])
+
+# print(mystr[15::2])
+
+# print(mystr[-15:30:2])
+
+# print(mystr[-7:-26:-3])
+
+# print(mystr[23:4:-3])
