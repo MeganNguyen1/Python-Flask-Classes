bc9024e69dbbf6f09cdb1e94662d109f91ad9d79
MeganNguyen1 <nguyenmegan973@gmail.com>
2023-11-21 19:41:43 -0600

    Set Topic is Done

--- set_practice.py
+++ set_practice.py
@@ -1,3 +1,27 @@
+myset1 = {1, 4, 5, 1, 1, 2, 4, 6, 7}
+myset2 = {3, 4, 4, 5, 9, 10, 3}
+
+myset3 = myset1.symmetric_difference(myset2)
+print(myset3)
+
+myset1.update(myset2)
+print(myset1)
+
+myset3 = myset1.intersection(myset2)
+print(myset3)
+
+print(myset1.isdisjoint(myset2))
+
+myset1.intersection_update(myset2)
+print(myset1)
+
+myset3 = myset1.union(myset2)
+print(myset3)
+
+mylist = [1, 5, 11, 8, 6, 32, 0, 13]
+myset1.update(mylist)
+print(myset1)
+
 # myset = {1, "red", 0, "hi", 45, "purple"}
 
 # for i in myset:
