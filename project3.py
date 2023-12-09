965f3ebedf9142c1896becff89e9f1c9893d24e7
MeganNguyen1 <nguyenmegan973@gmail.com>
2023-12-09 09:59:25 -0600

    Project 3: Food menu, Project 4: Star using Turtle

---
+++ project3.py
@@ -0,0 +1,32 @@
+print("Here are the food choices you have\n1. pizza\n2. Burger\n3. Salad\n4. Ice cream\n5. French Fries\n6. Brownie\n7. Sandwich\n8. Pasta\n9. Hotdog\n10. Soup")
+choice = input("What do you to have to eat?: ")
+
+if choice == "1":
+    print("Here! Have your pizza 🍕")
+
+if choice == "2":
+    print("Here! Have your burger 🍔")
+
+if choice == "3":
+    print("Here! Have your salad 🥗")
+
+if choice == "4":
+    print("Here! Have your ice cream 🍨")
+
+if choice == "5":
+    print("Here! Have your french fries 🍟")
+
+if choice == "6":
+    print("Here! Have your brownie 🍫")
+
+if choice == "7":
+    print("Here! Have your sandwich 🥪")
+
+if choice == "8":
+    print("Here! Have your pasta 🍝")
+
+if choice == "9":
+    print("Here! Have your hotdog 🌭")
+
+if choice == "10":
+    print("Here! Have your soup 🍲")
