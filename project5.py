30a04e4778c893e92d57796b404fb4e1dfd22263
MeganNguyen1 <nguyenmegan973@gmail.com>
2023-12-10 10:01:23 -0600

    Square, Rectangle, Heart

---
+++ project5.py
@@ -0,0 +1,36 @@
+import turtle
+# turtle.speed(4)
+# turtle.bgcolor("white")
+# turtle.pensize(5)
+# turtle.color("red")
+# turtle.fillcolor("red")
+# turtle.begin_fill()
+# turtle.left(50)
+# turtle.forward(133)
+# turtle.circle(50, 200)
+# turtle.right(140)
+# turtle.circle(50, 200)
+# turtle.forward(133)
+# turtle.end_fill()
+# turtle.hideturtle()
+# turtle.done()
+
+# my_turtle = turtle.Turtle()
+# turtle.speed(1)
+# for i in range(4):
+    # Forward function set the distance in units
+    # Right function sets the angle(direction)
+#     my_turtle.forward(250)
+#     my_turtle.right(90)
+# turtle.done()
+
+my_turtle = turtle.Turtle()
+turtle.speed(1)
+for i in range(2):
+    # Forward function set the distance in units
+    # Right function sets the angle(direction)
+    my_turtle.forward(150)
+    my_turtle.left(90)
+    my_turtle.forward(100)
+    my_turtle.left(90)
+turtle.done()
