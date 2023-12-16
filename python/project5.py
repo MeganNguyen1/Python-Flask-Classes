1eacf936e6e007c876fc28fb3d3a5b063fe0c7f7
MeganNguyen1 <nguyenmegan973@gmail.com>
2023-12-16 09:52:42 -0600

    Made Triangle, Circle, and Square using Turtle

--- python/project5.py
+++ python/project5.py
@@ -24,13 +24,47 @@ import turtle
 #     my_turtle.right(90)
 # turtle.done()
 
-my_turtle = turtle.Turtle()
-turtle.speed(1)
-for i in range(2):
+# my_turtle = turtle.Turtle()
+# turtle.speed(1)
+# for i in range(2):
     # Forward function set the distance in units
     # Right function sets the angle(direction)
-    my_turtle.forward(150)
-    my_turtle.left(90)
-    my_turtle.forward(100)
-    my_turtle.left(90)
-turtle.done()
+#     my_turtle.forward(150)
+#     my_turtle.left(90)
+#     my_turtle.forward(100)
+#     my_turtle.left(90)
+# turtle.done()
+
+import turtle
+# Square
+"""
+my_turtle = turtle.Turtle()
+my_turtle.speed(1)
+my_turtle.pensize(3)
+# pensize makes it bold
+my_turtle.fillcolor("lightblue")
+my_turtle.begin_fill()
+my_turtle.pencolor("blue")
+# my_turtle.forward(200)
+# Forward direction is from left to right
+# my_turtle.backward(350)
+# BAxkward direction is from right to left
+for i in range(4):
+    my_turtle.forward(250)
+    my_turtle.right(90)
+my_turtle.end_fill()
+"""
+
+# Triangle
+# my_turtle = turtle.Turtle()
+# my_turtle.speed(1)
+# for i in range(3):
+#     my_turtle.forward(150)
+#     my_turtle.right(120)
+
+# Circle
+my_turtle = turtle.Turtle()
+my_turtle.speed(7)
+for i in range(360):
+    my_turtle.forward(1)
+    my_turtle.right(1)
