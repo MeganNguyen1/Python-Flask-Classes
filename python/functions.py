9a5b33cd6295042059247d23a2525eb1315c9a40
MeganNguyen1 <nguyenmegan973@gmail.com>
2024-03-17 09:45:14 -0500

    more function based questions from leetcode

--- python/functions.py
+++ python/functions.py
@@ -9,3 +9,11 @@ class Solution(object):
         for i in range(len(nums)):
             mylist.append(nums[nums[i]])
         return mylist
+        
+class Solution(object):
+    def numberOfEmployeesWhoMetTarget(self, hours, target):
+        count = 0
+        for i in range(len(hours)):
+            if hours[i] >= target:
+                count += 1
+        return count
